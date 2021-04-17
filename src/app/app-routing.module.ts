3754c1f28d7969682c2627530f2347d5d27cc31f
iheb.benfraj <iheb.benfraj@notionedge.com>
2021-04-17 11:41:51 +0100

    feat: first commit v0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {ErrorComponent} from './components/error/error.component';
+import {EventComponent} from './components/event/event.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {path: 'home', component: HomeComponent},
+ {path: 'event', component: EventComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+
+
+  {path: 'error', component: ErrorComponent},
+
+  {path: '**', redirectTo: '/error', pathMatch: 'full'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import {APP_BASE_HREF} from '@angular/common';
+import { ErrorComponent } from './components/error/error.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
