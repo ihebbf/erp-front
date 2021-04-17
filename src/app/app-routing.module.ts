9b8c281e46624544528f51ddb80f9e7c47cb940a
ihebbf <36446204+ihebbf@users.noreply.github.com>
2021-04-17 12:19:28 +0100

    feat: add all components and services (#1)
    Co-authored-by: iheb.benfraj <iheb.benfraj@notionedge.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {ErrorComponent} from './components/error/error.component';
 import {EventComponent} from './components/event/event.component';
+import {ClubComponent} from './components/club/club.component';
+import {MeetingComponent} from './components/meeting/meeting.component';
+import {UserComponent} from './components/user/user.component';
 
 const routes: Routes = [  {path: 'home', component: HomeComponent},
- {path: 'event', component: EventComponent},
+  {path: 'event', component: EventComponent},
+  {path: 'club', component: ClubComponent},
+  {path: 'meeting', component: MeetingComponent},
+  {path: 'user', component: UserComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-
-
   {path: 'error', component: ErrorComponent},
-
   {path: '**', redirectTo: '/error', pathMatch: 'full'},
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { HomeComponent } from './components/home/home.component';
 import {APP_BASE_HREF} from '@angular/common';
 import { ErrorComponent } from './components/error/error.component';
 import { EventComponent } from './components/event/event.component';
+import { UserComponent } from './components/user/user.component';
+import { ClubComponent } from './components/club/club.component';
+import { MeetingComponent } from './components/meeting/meeting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ErrorComponent,
-    EventComponent
+    EventComponent,
+    UserComponent,
+    ClubComponent,
+    MeetingComponent
   ],
   imports: [
     BrowserModule,
